68aded006206dd201e02976ddbb30ef1b7680e29
roberticold <70712597+roberticold@users.noreply.github.com>
2021-05-12 09:27:35 -0500

    last changes

--- app/models.py
+++ app/models.py
@@ -5,20 +5,20 @@ from datetime import datetime
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(30), nullable=False, unique=True)
+    name = db.Column(db.String(30), nullable=False, unique=True, default="")
     last_name = db.Column(db.String(30), nullable=False, unique=True)
     phone = db.Column(db.String(17), nullable=False)
     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def __init__(self, name, last_name,phone):
         self.name = name
-        self.las_name = last_name
+        self.last_name = last_name
         self.phone = phone
 
     def __repr__(self):
         return f'<User | {self.name}>'
-        return f'<User | {self.las_name}>'
-        return f'<User | {self.phone}>'
+        # return f'<User | {self.las_name}>'
+        # return f'<User | {self.phone}>'
 
 
 class Post(db.Model):
